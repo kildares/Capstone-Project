5d03438463b847c1f4c70450884881fc3b734b21
Kildare Silveira <kildare.silveira@gmail.com>
2018-06-08 01:57:40 +0000

    Adding first screens

---
+++ app/src/main/java/futmatcher/kildare/com/futmatcher/MainActivity.java
@@ -0,0 +1,13 @@
+package futmatcher.kildare.com.futmatcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
